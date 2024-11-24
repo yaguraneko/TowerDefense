f6750089bcee38b6d5e3f6006039d7594a00be6e
neoAIH <abdelwahed.iraqui@gmail.com>
2024-11-24 22:38:06 +0100

    amenagement dossiers

---
+++ Assets/Shop.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+   /*  // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    } */
+    public void PurchaseArrowTower()
+    {
+        Debug.Log("Archer Preparé");
+    }
+}
