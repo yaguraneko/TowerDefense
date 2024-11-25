20fd5d6623328bbe2b74d9c09672f8c46c1b260a
neoAIH <abdelwahed.iraqui@gmail.com>
2024-11-25 18:05:05 +0100

    com

--- Assets/Script/Tower/Projectil.cs
+++ Assets/Script/Tower/Projectil.cs
@@ -39,7 +39,7 @@ public class Projectil : MonoBehaviour
         }
 
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
-       // transform.LookAt(target);
+        transform.LookAt(target);
        // transform.position = Vector3.MoveTowards(transform.position, target.position, distanceThisFrame);
        // Debug.Log("Projectile position: " + transform.position + " | Target position: " + target.position); 
         
